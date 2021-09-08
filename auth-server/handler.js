bab7860141f0fd334690d017bbf0d58e45886b2c
Amir Ernest <amir.m.ernest@gmail.com>
2021-09-08 21:10:20 +0200

    final get events function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -110,7 +110,7 @@ module.exports.getCalendarEvents = async (event) => {
       redirect_uris[0]
     );
 
-    const access_token = decodeURIComponent(`${event.pathParameters.code}`);
+    const access_token = decodeURIComponent(`${event.pathParameters.access_token}`) 
 
     oAuth2Client.setCredentials({ access_token });
 
